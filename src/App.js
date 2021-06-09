a20cf8bc6743be0e9699fa83f61c02677a410def
Gbemisola Kotoye <barkty@yahoo.com>
2021-06-09 11:26:55 +0100

    Index is important

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Fragment, useState, useEffect } from 'react';
-import 'index.css';
+import './index.css';
 import Form from './components/Form';
 import TaskList from './components/TaskList';
 
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Task = ({task}) => {
+    const [todo, setTodo] = useState(tasks);
+
+    const deleteTask = (index) => {
+	//Delete task
+	const removeTask = todo.filter((task) => task.index !== index);
+	setTodo(removeTask);
+    }
+    
     return(
 	<div className="task">
 	    <p className="taskItem">{task}</p>
+	    <button type="submit" onClick={() => deleteTask(index)}>Delete</button>
 	</div>
     );
 };
